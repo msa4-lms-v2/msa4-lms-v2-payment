7324aa6d5e13f1bcda31efb0a45a19310f9daf46
jinny-do <sonmojiri7@gmail.com>
2026-08-07 13:12:41 +0900

    first commit

---
+++ src/main/java/com/msa4lmsv2payment/Msa4LmsV2PaymentApplication.java
@@ -0,0 +1,13 @@
+package com.msa4lmsv2payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4LmsV2PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4LmsV2PaymentApplication.class, args);
+    }
+
+}
